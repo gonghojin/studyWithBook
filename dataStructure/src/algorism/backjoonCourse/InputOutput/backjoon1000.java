8028ff5bf88f4162ba311d111ee854d69691e50e
gong <yolip51@naver.com>
2018-05-20 16:06:14 +0900

    Add : IO algorism

---
+++ dataStructure/src/algorism/backjoonCourse/InputOutput/backjoon1000.java
@@ -0,0 +1,16 @@
+package algorism.backjoonCourse.InputOutput;
+
+import java.util.Scanner;
+
+public class backjoon1000 {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        int a, b;
+
+        a = sc.nextInt();
+        b = sc.nextInt();
+        System.out.println(a + b);
+    }
+}
+
---
+++ dataStructure/src/algorism/backjoonCourse/InputOutput/backjoon11721.java
@@ -0,0 +1,19 @@
+package algorism.backjoonCourse.InputOutput;
+
+import java.util.Scanner;
+
+public class backjoon11721 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String line = sc.nextLine();
+        sc.close();
+
+        for (int i = 0; i < line.length(); i++) {
+            if (i % 10 == 0 && i != 0) {
+                System.out.println();
+            }
+            System.out.print(line.charAt(i));
+
+        }
+    }
+}
