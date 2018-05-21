6a204c3b6f8aca9a2536eab79be3c87f25d3c6ff
gong <yolip51@naver.com>
2018-05-21 18:58:43 +0900

    Add : Stack algorithm(backjoon1158) & Fix : package's Name

---
+++ dataStructure/src/algorithm/baekjoonCourse/stack/backjoon9012.java
@@ -0,0 +1,38 @@
+package algorithm.baekjoonCourse.stack;
+
+import java.util.Scanner;
+
+public class backjoon9012 {
+    public static String isValid(String s) {
+        s = s.trim();
+        int n = s.length();
+        int cnt = 0;
+
+        for (int i = 0; i < n; i++) {
+
+            if (s.charAt(i) == '(') {
+                cnt++;
+            } else {
+                cnt--;
+            }
+            if (cnt < 0) {
+                return "NO";
+            }
+        }
+
+        if (cnt == 0) {
+            return "YES";
+        } else {
+            return "NO";
+        }
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        sc.nextLine();
+
+        while (n-- > 0) {
+            System.out.println(isValid(sc.nextLine()));
+        }
+    }
+}
