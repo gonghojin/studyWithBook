29a67d66895d5059f89a602ac627f9e641928ca5
gong <yolip51@naver.com>
2018-05-22 22:48:42 +0900

    Add : baekjoon(11726,9095) - D.P

---
+++ dataStructure/src/algorithm/baekjooncourse/dp/baekjoon11726.java
@@ -0,0 +1,28 @@
+package algorithm.baekjooncourse.dp;
+
+import java.util.Scanner;
+
+// https://www.acmicpc.net/problem/11726
+public class baekjoon11726 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
+        int[] d = new int[1001]; // 1번 인덱스부터 시작
+        d[0] = 1; // 이치상은 당연히 없는 타일은 1이 아니지만, 없지만 1이라고 놓고 계산하면 알고리즘의 원리 이해가 쉽다
+        d[1] = 1;
+        for (int i = 2; i <= num; i++) {
+            d[i] = d[i -1] + d[i -2];
+            d[i] %= 10007;
+        }
+        System.out.println(d[num]);
+    }
+}
+    /**
+     *  DP문제의 특징을 간파하자!
+     *  작은 문제의 합은 큰 문제로 도출된다.
+     *  작게 보고 풀면 문제를 풀 수 있다.
+     *  1. Overlapping Subproblem
+     *  2. Optimal Substructure
+     *
+     *
+     * **/
---
+++ dataStructure/src/algorithm/baekjooncourse/dp/baekjoon9095.java
@@ -0,0 +1,26 @@
+package algorithm.baekjooncourse.dp;
+
+import java.util.Scanner;
+
+// https://www.acmicpc.net/problem/9095
+public class baekjoon9095 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int[] d = new int[11]; // 인덱스 1부터 시작 11보다 작다니
+                                //d[n] = n을 1, 2, 3 조합으로 나타내는 방법의 수
+        d[0] = 1;
+        for (int i = 1; i <= 10; i++) {
+            for (int j = 1; j <= 3; j++) {
+                if (i >= j) {
+                    d[i] += d[i - j]; // d[i] = d[i - 1] + d[i -2] + d[i -3]
+                }                     // 마지막 자리가 1인 d[i]의 앞자리 경우의 수는 d[i -1] = d[i]
+            }                         // 2의 경우 = d[i - 2], 3 = d[i - 3]
+        }
+
+        int tastCase = sc.nextInt();
+        while (tastCase-- > 0){
+            int num = sc.nextInt();
+            System.out.println(d[num]);
+        }
+    }
+}
