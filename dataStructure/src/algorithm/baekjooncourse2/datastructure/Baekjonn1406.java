a3ac75f32a1d43c81cb5d150700f05877812af92
gong <yolip51@naver.com>
2018-06-27 22:47:50 +0900

    Add : baekjoon(1406) 2회 - 에디터(스택)

---
+++ dataStructure/src/algorithm/baekjooncourse2/datastructure/Baekjonn1406.java
@@ -0,0 +1,50 @@
+package algorithm.baekjooncourse2.datastructure;
+
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Stack;
+
+public class Baekjonn1406 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String line = br.readLine();
+        Stack<Character> left = new Stack<Character>();
+        Stack<Character> right = new Stack<Character>();
+        for (int i = 0; i < line.length(); i++) {
+            left.push(line.charAt(i));
+        }
+
+        int cmdCount = Integer.parseInt(br.readLine());
+        while (cmdCount-- > 0) {
+            String[] cmdArr = br.readLine().split(" ");
+            char cmd = cmdArr[0].charAt(0);
+            if (cmd == 'L') {
+                if (!left.empty()) {
+                    right.push(left.pop());
+                }
+            } else if (cmd == 'D') {
+                if (!right.empty()) {
+                    left.push(right.pop());
+                }
+            } else if (cmd == 'B') {
+                if (!left.empty()) {
+                    left.pop();
+                }
+            } else if (cmd == 'P') {
+                char input = cmdArr[1].charAt(0);
+                left.push(input);
+            }
+        }
+        while (!left.empty()) {
+            right.push(left.pop());
+        }
+        StringBuilder sb = new StringBuilder();
+        while (!right.empty()) {
+            sb.append(right.pop());
+        }
+        System.out.println(sb);
+    }
+}
