13ac931425d6a33487567b2f10ab392df7e4f2f8
gong <yolip51@naver.com>
2018-07-18 23:59:33 +0900

    Fix : baekjoon(9461) - 파도반 수열[다른 방법]

--- dataStructure/src/algorithm/baekjooncourse2/dynamic/Baekjoon9461.java
+++ dataStructure/src/algorithm/baekjooncourse2/dynamic/Baekjoon9461.java
@@ -1,19 +1,18 @@
 package algorithm.baekjooncourse2.dynamic;
 
 import java.util.*;
-import java.math.*;
 
 public class Baekjoon9461 {
     public static void main(String args[]) {
         Scanner sc = new Scanner(System.in);
         long[] d = {0, 1, 1, 1, 2, 2, 3, 4, 5, 7, 9};
         d = Arrays.copyOf(d, 101);
-        for (int i = 10; i <= 100; i++) {
-            d[i] = d[i - 1] + d[i - 5];
-        }
         int t = sc.nextInt();
         while (t-- > 0) {
             int n = sc.nextInt();
+            for (int i = 11; i <= n; i++) {
+                d[i] = d[i-1] + d[i-5];
+            }
             System.out.println(d[n]);
         }
     }
