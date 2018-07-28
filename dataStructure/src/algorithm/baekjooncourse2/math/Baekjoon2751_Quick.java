9c7f145903cf9a0a17d6d572e3e0424e427f8e88
gong <yolip51@naver.com>
2018-07-28 11:01:32 +0900

    Add : baekjoon(2751) - 수정렬_퀵정렬

--- dataStructure/src/algorithm/baekjooncourse2/math/Baekjoon2751_Quick.java
+++ dataStructure/src/algorithm/baekjooncourse2/math/Baekjoon2751_Quick.java
@@ -5,54 +5,54 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class Baekjoon2751_Quick {
-    public static void swap(int[] arr, int idx1, int idx2) {
-        int temp = arr[idx1];
-        arr[idx1] = arr[idx2];
-        arr[idx2] = temp;
+
+    private static void swap(int[] arr, int low, int right) {
+        int temp = arr[low];
+        arr[low] = arr[right];
+        arr[right] = temp;
     }
 
-    public static int partion(int arr[], int left, int right) {
-        // 피벗 위치 가장 왼쪽
+    public static void quickSort(int[] arr, int left, int right) {
+        if (right <= left) {
+            return;
+        }
         int pivot = arr[left];
         int low = left + 1;
         int high = right;
 
         while (low <= high) {
-
-            // 피벗보다 큰 값을 찾는 과정
-            while (arr[low] <= pivot && low < right) {
+            while (low <= right && arr[low] <= pivot) {
                 low++;
             }
-
-            // 피벗보다 작은 값
-            while (pivot <= arr[high] && (left + 1) <= high) {
+            while (left + 1 <= high && pivot <= arr[high]) {
                 high--;
             }
-
-            if (low <= high) {
+            if (low < high) {
                 swap(arr, low, high);
             }
         }
         swap(arr, left, high);
-        return high;
+        quickSort(arr, left, high - 1);
+        quickSort(arr, high + 1, right);
     }
-    public static void quickSort(int[] arr,int left, int right) {
-        if (left <= right) {
-            int pivot = partion(arr, left, right);
-            quickSort(arr, left, pivot - 1);
-            quickSort(arr, pivot + 1, right);
-        }
-    }
+
+
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int n = Integer.parseInt(br.readLine().trim());
-        int[] arr = new int[n];
-        for (int i = 0; i < n; i++) {
+        StringBuilder sb = new StringBuilder();
+        int N = Integer.parseInt(br.readLine().trim());
+        int arr[] = new int[N];
+
+        for (int i = 0; i < N; i++)
             arr[i] = Integer.parseInt(br.readLine().trim());
-        }
-        quickSort(arr, 0, n - 1);
-        for (int i = 0; i < n; i++) {
-            System.out.println(arr[i]);
-        }
+
+        quickSort(arr, 0, N - 1);
+
+        for (int i = 0; i < N; i++)
+            sb.append(arr[i] + "\n");
+
+        System.out.println(sb);
+
     }
 }
