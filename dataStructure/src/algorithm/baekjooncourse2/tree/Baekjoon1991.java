631eec8ccc6d26fe9236ad8281f2fa08d5f620d9
gong <yolip51@naver.com>
2018-08-16 21:51:39 +0900

    Add : baekjoon(1991) - 트리순회[이진트리]

---
+++ dataStructure/src/algorithm/baekjooncourse2/tree/Baekjoon1991.java
@@ -0,0 +1,63 @@
+package algorithm.baekjooncourse2.tree;
+
+import java.util.Scanner;
+
+public class Baekjoon1991 {
+    public static void preorder(int[][] a, int x) {
+        if (x == -1) {
+            return;
+        }
+        System.out.print((char) (x + 'A'));
+        preorder(a, a[x][0]);
+        preorder(a, a[x][1]);
+    }
+
+    public static void inorder(int[][] a, int x) {
+        if (x == -1) {
+            return;
+        }
+        inorder(a, a[x][0]);
+        System.out.print((char) (x + 'A'));
+        inorder(a, a[x][1]);
+    }
+
+    public static void postorder(int[][] a, int x) {
+        if (x == -1) {
+            return;
+        }
+        postorder(a, a[x][0]);
+        postorder(a, a[x][1]);
+        System.out.print((char) (x + 'A'));
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        sc.nextLine();
+        int[][] a = new int[26][2];
+        for (int i = 0; i < n; i++) {
+            String line = sc.nextLine();
+            int x = line.charAt(0) - 'A';
+            char l = line.charAt(2);
+            char r = line.charAt(4);
+
+            if (l == '.') {
+                a[x][0] = -1;
+            } else {
+                a[x][0] = l - 'A';
+            }
+            if (r == '.') {
+                a[x][1] = -1;
+            } else {
+                a[x][1] = r - 'A';
+            }
+        }
+
+        preorder(a, 0);
+        System.out.println();
+        inorder(a, 0);
+        System.out.println();
+        postorder(a, 0);
+        System.out.println();
+    }
+}
