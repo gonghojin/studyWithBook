9d236ef5bf1231ce363a8744f405bf2015a3aa6f
gonghojin <yolip51@naver.com>
2018-09-01 17:42:15 +0900

    Add : Baekjoon10844[쉬운 계단 수 오르기]

---
+++ dataStructure/src/algorithm/baekjooncourse3/dp/Baekjoon10844.java
@@ -0,0 +1,33 @@
+package algorithm.baekjooncourse3.dp;
+
+import java.util.Scanner;
+
+public class Baekjoon10844 {
+    public static final int mod = 1000000000;
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        long[][] d = new long[n + 1][10];
+        for (int i = 1; i <= 9; i++) {
+            d[1][i] = 1;
+        }
+        for (int i = 2; i <= n; i++) {
+            for (int j = 0; j <= 9; j++) {
+                if (j - 1 >= 0) {
+                    d[i][j] += d[i - 1][j - 1];
+                }
+                if (j + 1 <= 9) {
+                    d[i][j] += d[i - 1][j + 1];
+                }
+                d[i][j] %= mod;
+            }
+        }
+        long ans = 0;
+        for (int i = 0; i <= 9; i++) {
+            ans += d[n][i];
+        }
+        ans %= mod;
+        System.out.println(ans);
+    }
+}
