330b34a13d2de6afbc4843c90b735a9fbfb719e4
gonghojin <yolip51@naver.com>
2018-09-08 14:47:06 +0900

    Add : baekjoon11722[가장 긴 감소하는 부분 수열-dp]

---
+++ dataStructure/src/algorithm/baekjooncourse3/dp/Baekjoon11722.java
@@ -0,0 +1,31 @@
+package algorithm.baekjooncourse3.dp;
+
+import java.util.Scanner;
+
+public class Baekjoon11722 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] a = new int[n];
+        for (int i = n - 1; 0 <= i; i--) {
+            a[i] = sc.nextInt();
+        }
+
+        int[] d = new int[n];
+        for (int i = 0; i < n; i++) {
+            d[i] = 1;
+            for (int j = 0; j < i; j++) {
+                if (a[j] < a[i] && d[i] < d[j] + 1) {
+                    d[i] = d[j] + 1;
+                }
+            }
+        }
+        int ans = d[0];
+        for (int i = 1; i < n; i++) {
+            if (ans < d[i]) {
+                ans = d[i];
+            }
+        }
+        System.out.println(ans);
+    }
+}
