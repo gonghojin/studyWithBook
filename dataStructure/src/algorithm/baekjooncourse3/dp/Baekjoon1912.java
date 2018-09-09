48d7afae13e91443dd9f8b74e038d761accf4170
gonghojin <yolip51@naver.com>
2018-09-09 11:31:39 +0900

    Add : baekjoon1912[연속합-dp]

---
+++ dataStructure/src/algorithm/baekjooncourse3/dp/Baekjoon1912.java
@@ -0,0 +1,36 @@
+package algorithm.baekjooncourse3.dp;
+
+import java.util.Scanner;
+
+public class Baekjoon1912 {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] a = new int[n];
+        for (int i = 0; i < n; i++) {
+            a[i] = sc.nextInt();
+        }
+
+        int[] d = new int[n];
+        for (int i = 0; i < n; i++) {
+            d[i] = a[i];
+            if (i == 0) {
+                continue;
+            }
+            if (d[i] < d[i - 1] + a[i]) {
+                d[i] = d[i - 1] + a[i];
+            }
+
+        }
+
+        int ans = d[0];
+        for (int i = 1; i < n; i++) {
+            if (ans < d[i]) {
+                ans = d[i];
+            }
+        }
+        System.out.println(ans);
+    }
+}
+
