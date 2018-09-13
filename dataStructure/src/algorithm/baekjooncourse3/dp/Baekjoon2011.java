d23b451031fb7e8b76a6d622f01ba63d413d9d6c
gong <yolip51@naver.com>
2018-09-13 23:50:56 +0900

    Add : baekjoon(2011) - 암호코드[dp]

---
+++ dataStructure/src/algorithm/baekjooncourse3/dp/Baekjoon2011.java
@@ -0,0 +1,34 @@
+package algorithm.baekjooncourse3.dp;
+
+import java.util.Scanner;
+
+public class Baekjoon2011 {
+    public static int mod = 1000000;
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String s = sc.nextLine().trim();
+        int sL = s.length();
+        s = " " + s;
+        int[] d = new int[sL + 1];
+        d[0] = 1;
+        for (int i = 1; i <= sL; i++) {
+            int x = s.charAt(i) - '0';
+            if (1 <= x && x <= 9) {
+                d[i] += d[i - 1];
+                d[i] %= mod;
+            }
+            if (i == 1) {
+                continue;
+            }
+            if (s.charAt(i - 1) == '0') {
+                continue;
+            }
+            x = (s.charAt(i - 1)  - '0') * 10 + (s.charAt(i) - '0');
+            if (10 <= x && x <= 26) {
+                d[i] += d[i - 2];
+                d[i] %= mod;
+            }
+        }
+        System.out.println(d[sL]);
+    }
+}
