aca9ce07c1120c383473bf95d580d118fdeff22b
gonghojin <yolip51@naver.com>
2018-09-23 13:40:38 +0900

    Add : baekjoon(11653)- 소인수 분해

---
+++ dataStructure/src/algorithm/baekjooncourse3/Baekjoon11653.java
@@ -0,0 +1,20 @@
+package algorithm.baekjooncourse3;
+
+import java.util.Scanner;
+
+public class Baekjoon11653 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        for (int i = 2; i * i <= n; i++) {
+            while (n % i == 0) {
+                System.out.println(i);
+                n /= i;
+            }
+        }
+        if (1 < n) {
+            System.out.println(n);
+        }
+    }
+
+}
