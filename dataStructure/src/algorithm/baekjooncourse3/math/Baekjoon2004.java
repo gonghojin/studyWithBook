ed1492f2d0ce056e3af6d646d345d2b014998add
gonghojin <yolip51@naver.com>
2018-09-23 16:38:22 +0900

    Add : baekjoon(2004)- 조합 0의 개수

---
+++ dataStructure/src/algorithm/baekjooncourse3/math/Baekjoon2004.java
@@ -0,0 +1,36 @@
+package algorithm.baekjooncourse3.math;
+
+import java.util.Scanner;
+
+public class Baekjoon2004 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        long n = sc.nextLong();
+        long m = sc.nextLong();
+
+        long two = 0, five = 0;
+        for (long i = 2; i <= m; i *= 2) {
+            two -= m / i;
+        }
+        for (long i = 5; i <= m; i *= 5) {
+            five -= m / i;
+        }
+
+        for (long i = 2; i <= (n - m); i *= 2) {
+            two -= (n - m) / i;
+        }
+        for (long i = 5; i <= (n - m); i *= 5) {
+            five -= (n - m) / i;
+        }
+
+        for (long i = 2; i <= n; i *= 2) {
+            two += n / i;
+        }
+        for (long i = 5; i <= n; i *= 5) {
+            five += n / i;
+        }
+
+        System.out.println(two <= five? two: five);
+    }
+}
