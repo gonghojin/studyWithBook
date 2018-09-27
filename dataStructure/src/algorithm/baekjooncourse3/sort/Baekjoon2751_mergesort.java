95b214e1a1b522cf6b0da27c48117fcc5a571856
gong <yolip51@naver.com>
2018-09-27 23:53:27 +0900

    Add : baekjoon(2751)- 병합정렬(완성)

--- dataStructure/src/algorithm/baekjooncourse3/sort/Baekjoon2751_mergesort.java
+++ dataStructure/src/algorithm/baekjooncourse3/sort/Baekjoon2751_mergesort.java
@@ -7,14 +7,63 @@ import java.util.StringTokenizer;
 
 public class Baekjoon2751_mergesort {
 
+    public static int[] result;
+
+    public static void mergeSort(int[] arr, int left, int right) {
+        if (left < right) {
+            int mid = (left + right) / 2;
+            mergeSort(arr, left, mid);
+            mergeSort(arr, mid + 1, right);
+            merge(arr, left, mid, right);
+        }
+    }
+
+    private static void merge(int[] arr, int left, int mid, int right) {
+        int fIdx = left;
+        int rIdx = mid + 1;
+        int sIdx = left;
+
+        while (fIdx <= mid && rIdx <= right) {
+            if (arr[fIdx] <= arr[rIdx]) {
+                result[sIdx++] = arr[fIdx++];
+            } else {
+                result[sIdx++] = arr[rIdx++];
+            }
+        }
+
+        if (fIdx <= mid) {
+            for (int i = fIdx; i <= mid; i++) {
+                result[sIdx++] = arr[i];
+            }
+        } else {
+            for (int i = rIdx; i <= right; i++) {
+                result[sIdx++] = arr[i];
+            }
+        }
+
+        for (int i = left; i <= right; i++) {
+            arr[i] = result[i];
+        }
+    }
+
     public static void main(String[] args) {
 
         sc.init();
+
         int n = sc.nextInt();
         int[] arr = new int[n];
+        result = new int[n];
+
         for (int i = 0; i < n; i++) {
             arr[i] = sc.nextInt();
         }
+        mergeSort(arr, 0, n -1);
+
+        StringBuffer sb = new StringBuffer();
+        for (int i = 0; i < n; i++) {
+            sb.append(result[i] + "\n");
+        }
+        System.out.println(sb);
     }
 
     static class sc {
