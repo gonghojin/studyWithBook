111000d6854b57d09ea0164f16869f70d059c51a
gonghojin <yolip51@naver.com>
2018-09-29 15:46:24 +0900

    Add : baekjoon(11650)- 좌표 정렬

---
+++ dataStructure/src/algorithm/baekjooncourse3/sort/Baekjoon11650.java
@@ -0,0 +1,34 @@
+package algorithm.baekjooncourse3.sort;
+
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.Scanner;
+
+public class Baekjoon11650 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[][] arr = new int[n][2];
+        for (int i = 0 ; i < n; i++) {
+            arr[i][0] = sc.nextInt();
+            arr[i][1] = sc.nextInt();
+        }
+
+        Arrays.sort(arr, new Comparator<int[]>() {
+            @Override
+            public int compare(int[] o1, int[] o2) {
+                if (o1[0] == o2[0]) {
+                    return Integer.compare(o1[1], o2[1]);
+                } else {
+                    return Integer.compare(o1[0], o2[0]);
+                }
+
+            }
+        });
+
+        for (int[] a : arr) {
+            System.out.println(a[0] + " " + a[1]);
+        }
+    }
+}
