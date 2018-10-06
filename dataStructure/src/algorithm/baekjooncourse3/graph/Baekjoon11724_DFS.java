73037fa7003fcf9b5ab91cb8a1131e8c5a1ca8f8
gonghojin <yolip51@naver.com>
2018-10-06 12:00:24 +0900

    Add : baekjoon(11724)- 연결 요소[DFS]

---
+++ dataStructure/src/algorithm/baekjooncourse3/graph/Baekjoon11724_DFS.java
@@ -0,0 +1,82 @@
+package algorithm.baekjooncourse3.graph;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.StringTokenizer;
+
+public class Baekjoon11724_DFS {
+
+    private static ArrayList<Integer>[] d;
+
+    private static void dfs(boolean[] check, int u) {
+        if (check[u]) {
+            return ;
+        }
+
+        check[u] = true;
+        for (int x : d[u]) {
+            if (check[x] == false) {
+                dfs(check, x);
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        sc.init();
+
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+
+        d = (ArrayList<Integer>[]) new ArrayList[n + 1];
+        for (int i = 1; i <= n; i++) {
+            d[i] = new ArrayList<Integer>();
+        }
+
+        for (int i = 0; i < m; i++) {
+            int u = sc.nextInt();
+            int v = sc.nextInt();
+            d[u].add(v);
+            d[v].add(u);
+        }
+
+        int component = 0;
+        boolean[] check = new boolean[n + 1];
+        for (int i = 1; i <= n; i++) {
+            if (check[i] == false) {
+                component++;
+                dfs(check, i);
+            }
+        }
+
+        System.out.println(component);
+    }
+
+    static class sc {
+        private static BufferedReader br;
+        private static StringTokenizer st;
+
+        public static void init() {
+            br = new BufferedReader(new InputStreamReader(System.in));
+            st = new StringTokenizer("");
+        }
+
+        private static String next() {
+            if (!st.hasMoreTokens()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+
+            }
+
+            return st.nextToken();
+        }
+
+        public static int nextInt() {
+            return Integer.parseInt(next());
+        }
+    }
+}
