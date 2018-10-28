1cbcd8b241637cf93301973d503f72a08458cd8d
gonghojin <yolip51@naver.com>
2018-10-28 10:57:47 +0900

    Add : baekjoon(1783) - 병든 나이트[그리디] 다시풀기

--- dataStructure/src/algorithm/baekjooncourse/greedy/Baekjoon1783.java
+++ dataStructure/src/algorithm/baekjooncourse/greedy/Baekjoon1783.java
@@ -1,50 +1,23 @@
 package algorithm.baekjooncourse.greedy;
 
-import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Baekjoon1783 {
+    public static void main(String[] args) {
+       Scanner sc = new Scanner(System.in);
+       int height = sc.nextInt();
+       int width = sc.nextInt();
 
-    public static void main(String args[]) {
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        int m = sc.nextInt();
-        int k = sc.nextInt();
-        if (m + k - 1 <= n && n <= m * k) {
-            int[] a = new int[n];
-            for (int i = 0; i < n; i++) {
-                a[i] = i + 1;
-            }
-            ArrayList<Integer> group = new ArrayList<Integer>();
-            group.add(0);
-            group.add(k);
-            n -= k;
-            m -= 1;
-            int group_size = m == 0 ? 1 : n / m;
-            int r = m == 0 ? 0 : n % m;
-            for (int i = 0; i < m; i++) {
-                group.add(group.get(group.size() - 1) + group_size + (r > 0 ? 1 : 0));
-                if (r > 0) {
-                    r -= 1;
-                }
-            }
-            for (int i = 0; i < group.size() - 1; i++) {
-                int begin = group.get(i);
-                int end = group.get(i + 1) - 1;
-                while (begin < end) {
-                    int temp = a[begin];
-                    a[begin] = a[end];
-                    a[end] = temp;
-                    begin += 1;
-                    end -= 1;
-                }
-            }
-            for (int i = 0; i < a.length; i++) {
-                System.out.print(a[i] + " ");
-            }
-            System.out.println();
-        } else {
-            System.out.println(-1);
-        }
+       if (height == 1) { // 이동 못함
+           System.out.println(1);
+       } else if(height == 2) {
+           System.out.println(Math.min(4, (width) / 2));
+       } else if (3 <= height) {
+           if (7 <= width) {
+               System.out.println(width - 2);
+           } else {
+               System.out.println(Math.min(4, width));
+           }
+       }
     }
 }
