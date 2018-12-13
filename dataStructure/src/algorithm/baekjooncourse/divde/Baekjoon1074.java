10283a06ef0d734df135a22155de1a421aff9637
gonghojin <yolip51@naver.com>
2018-12-13 23:31:03 +0900

    Add : baekjoon(1074) - Z  [분할정복]  content : 블로그 글 보고 다시해보기, 분할정복은 재귀함수의 응용이다. 재귀함수는 난이도가 있는 대표 문제를 이해암기가 필요하다

---
+++ dataStructure/src/algorithm/baekjooncourse/divde/Baekjoon1074.java
@@ -0,0 +1,48 @@
+package algorithm.baekjooncourse.divde;
+
+import java.util.Scanner;
+
+//http://mygumi.tistory.com/284
+public class Baekjoon1074 {
+
+    static void solve(int n, int r, int c) {
+        int x = (int) Math.pow(2, n) / 2;
+        int y = x;
+        int ans = 0;
+
+        while (0 < n--) {
+            int temp = (int) Math.pow(2, n) / 2;
+            int skip = (int) Math.pow(4, n);
+
+            if (r < y && c < x) {
+                // 1
+                x -= temp;
+                y -= temp;
+            } else if (r < y && x <= c) {
+                // 2
+                x += temp;
+                y -= temp;
+                ans += skip;
+            } else if (y <= r && c < x) {
+                // 3
+                x -= temp;
+                y += temp;
+                ans += skip * 2;
+            } else {
+                // 4
+                x += temp;
+                y += temp;
+                ans += skip * 3;
+            }
+        }
+
+        System.out.println(ans);
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int r = sc.nextInt();
+        int c = sc.nextInt();
+        solve(n, r, c);
+    }
+}
