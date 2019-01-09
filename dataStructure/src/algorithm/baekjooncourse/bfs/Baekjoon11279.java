dd21a779a6da7fcd7a0913b6bd24cc148d082e0b
gonghojin <yolip51@naver.com>
2019-01-09 23:25:58 +0900

    Add : 힙 테스트

---
+++ dataStructure/src/algorithm/baekjooncourse/bfs/Baekjoon11279.java
@@ -0,0 +1,35 @@
+package algorithm.baekjooncourse.bfs;
+
+import java.util.Comparator;
+import java.util.PriorityQueue;
+import java.util.Scanner;
+
+public class Baekjoon11279 {
+
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+
+        Compare cmp = new Compare();
+        PriorityQueue<Integer> q = new PriorityQueue<Integer>(1, cmp);
+
+        int n = sc.nextInt();
+        while (n-- > 0) {
+            int x = sc.nextInt();
+            if (x == 0) {
+                if (q.isEmpty()) {
+                    System.out.println(0);
+                } else {
+                    System.out.println(q.poll());
+                }
+            } else {
+                q.offer(x);
+            }
+        }
+    }
+
+    static class Compare implements Comparator<Integer> {
+        public int compare(Integer one, Integer two) {
+            return two.compareTo(one);
+        }
+    }
+}
