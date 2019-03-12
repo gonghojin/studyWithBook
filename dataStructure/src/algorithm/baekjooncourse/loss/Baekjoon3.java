7f6adc46db831daf8254ffb76ca0c6d9d351479d
gonghojin <yolip51@naver.com>
2019-03-12 18:53:02 +0900

    Add: 자료구조_1

---
+++ dataStructure/src/algorithm/baekjooncourse/loss/Baekjoon3.java
@@ -0,0 +1,21 @@
+package algorithm.baekjooncourse.loss;
+
+import java.util.Scanner;
+
+public class Baekjoon3 {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        String s = sc.nextLine();
+
+        int[] cnt = new int[26];
+        for (int i = 0; i < s.length(); i++) {
+            cnt[s.charAt(i) - 'a'] += 1;
+        }
+
+        for (int i = 0; i < 26; i++) {
+            System.out.print(cnt[i] + " ");
+        }
+
+        System.out.println();
+    }
+}
