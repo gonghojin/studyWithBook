eb3b3225b67a549c1de63f71aca3b0991af79ea9
gonghojin <yolip51@naver.com>
2019-04-13 14:37:34 +0900

    Add: 액션 생성  Content : 상수 및 액션 상수 만들어서 번거러움 줄이기

---
+++ React/redux-counter/src/actions/ActionTypes.js
@@ -0,0 +1,11 @@
+/*
+    Action 종류들을 선언.
+    앞에 export를 붙이면 나중에 이것들을 불러올 때,
+    import * as type from './ActionTypes' 를 할 수 있따.
+ */
+
+// Action을 선언할 때는 상수로 선언(대문자)
+export const INCREEMNT = 'INCREMENT';
+export const DECREMENT = 'DECREMENT';
+export const SET_COLOR = 'SET_COLOR';
+
---
+++ React/redux-counter/src/actions/index.js
@@ -0,0 +1,18 @@
+/*
+    action 객체를 만드는 액션 생성함수들을 선언
+    여기에서 () => ({}) 은 function () {return {}}와 동일
+ */
+
+import * as types from './ActionTypes';
+
+export const increment = () => ({
+    type: types.INCREEMNT,
+});
+export const decrement = () => ({
+    type: types.DECREMENT,
+});
+// 파라미터를 가지고 있는 액션
+export const increment = (color) => ({
+    type: types.SET_COLOR,
+    color,
+});
