cfcf3b3748c236e1b94ea15994919b601e478efb
gonghojin <hjgong@nkia.co.kr>
2019-04-27 22:36:03 +0900

    Add: 리덕스 미들웨어 개념 및 외부 데이터 연동 작업 2  - 오픈 소스 로거 사용(redux-logger)

--- React/redux-starter/src/store.js
+++ React/redux-starter/src/store.js
@@ -1,9 +1,17 @@
-import { createStore, applyMiddleware} from 'redux';
-import modules from './modules';
-import loggerMiddleware from './lib/loggerMiddleware';
+import { createStore, applyMiddleware } from 'redux';
+import modules from './modules'
 
-// 미들웨어가 여러 개일 떄는 파라미터로 전달하면 된다. 예 applyMiddleware(a, b, c)
-// 미들웨어 순서는 여기에서 전달한 파라미터 순서대로 지정한다.
-const store = createStore(modules, applyMiddleware(loggerMiddleware))
+/*
+    yarn add redux-logger
+ */
+import { createLogger } from 'redux-logger'
+
+/*
+    로그 미들웨어 설정 커스터마이징
+    https://github.com/LogRocket/redux-logger#options
+ */
+const logger = createLogger();
+
+const store = createStore(modules, applyMiddleware(logger));
 
 export default store;
