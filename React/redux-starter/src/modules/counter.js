91fa195eb5e7a9487a4a9d45d5bafe80dd6c416f
gonghojin <hjgong@nkia.co.kr>
2019-04-28 18:23:20 +0900

    Add: 리덕스 미들웨어 개념 및 외부 데이터 연동 작업 2  - 오픈 소스 비동기 사용(redux-thunk)

--- React/redux-starter/src/modules/counter.js
+++ React/redux-starter/src/modules/counter.js
@@ -1,4 +1,4 @@
-import {handleActions, createAction} from "redux-actions";
+import {createAction, handleActions} from "redux-actions";
 
 // 모듈 개수가 하나이기 떄문에 액션 이름 부분의 접두사 생략(counter/~)
 const INCREMENT = 'INCREMENT';
@@ -11,3 +11,20 @@ export default handleActions({
     [INCREMENT]: (state, action) => state + 1,
     [DECREMENT]: (state, action) => state - 1,
 }, 0);
+
+// tag - 카운터를 비동적으로 생성 추가(redux-thunk)
+export const incrementAsync = () => dispatch => {
+    // 1초 뒤 액션 디스패치
+    setTimeout(
+        () => {
+            dispatch(increment())
+        }, 1000);
+}
+
+export const decrementAsync = () => dispatch => {
+    // 1초 뒤 액션 디스패치
+    setTimeout(
+        () => {
+            dispatch(decrement())
+        }, 1000);
+}
--- React/redux-starter/src/store.js
+++ React/redux-starter/src/store.js
@@ -7,11 +7,16 @@ import modules from './modules'
 import { createLogger } from 'redux-logger'
 
 /*
+    yarn add redux-thunk
+ */
+import ReduxThunk from "redux-thunk";
+
+/*
     로그 미들웨어 설정 커스터마이징
     https://github.com/LogRocket/redux-logger#options
  */
 const logger = createLogger();
 
-const store = createStore(modules, applyMiddleware(logger));
+const store = createStore(modules, applyMiddleware(logger, ReduxThunk));
 
 export default store;
