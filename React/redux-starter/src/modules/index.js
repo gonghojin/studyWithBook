005f69d5eb43c5a8f4443505ab4c854e3eeac244
gonghojin <hjgong@nkia.co.kr>
2019-04-29 23:10:32 +0900

    Fix: 리덕스 미들웨어 개념 및 외부 데이터 연동 작업 2  - 오픈 소스 비동기 사용 4[thunk, promise-middleware 대신 pender 사용(추천)] (redux-pender)

--- React/redux-starter/src/modules/index.js
+++ React/redux-starter/src/modules/index.js
@@ -1,8 +1,13 @@
 import { combineReducers } from 'redux';
 import counter from './counter';
-import post from "./post";
+import post from "./~post";
+import {penderReducer} from "redux-pender";
 
 export default combineReducers({
     counter,
     post,
+    /*
+        미들웨어를 적용하고 난 후에는 라이브러리 안에 내장된 리듀서를 루트 리듀서에 넣어 준다.
+     */
+    pender: penderReducer
 });
--- React/redux-starter/src/store.js
+++ React/redux-starter/src/store.js
@@ -1,10 +1,10 @@
-import { createStore, applyMiddleware } from 'redux';
+import {createStore, applyMiddleware} from 'redux';
 import modules from './modules'
 
 /*
     yarn add redux-logger
  */
-import { createLogger } from 'redux-logger'
+import {createLogger} from 'redux-logger'
 
 /*
     비동기 작업 처리 방법 1 - yarn add redux-thunk
@@ -15,13 +15,24 @@ import ReduxThunk from "redux-thunk";
     비동기 작업 처리 방법 2 - yarn add redux-promise-middleware
  */
 /**
- * 이 미들웨어는 Promise 객체를 payload로 전달하면(post.js 참조) 요청을 시작 - 성공 - 실패할 때
+ * 이 미들웨어는 Promise 객체를 payload로 전달하면(~post.js 참조) 요청을 시작 - 성공 - 실패할 때
  * 액션의 뒷부분에 _PENDING, _FUFILED, _REJECTED를 붙어서 반환한다.
  * 따라서 각 액션 타입을 일일이 선언할 필요가 없다.
  * 뒷부분에 붙는 접미사는 커스터마이징할 수도 있다.(아래의  promiseTypeSuffixes: ['PENDING', 'SUCCESS', 'FAILURE'],)
  *
+ import promiseMiddleware from 'redux-promise-middleware';
+
+
+ const pm = promiseMiddleware({
+    promiseTypeSuffixes: ['PENDING', 'SUCCESS', 'FAILURE'],
+});
+
  */
-import promiseMiddleware from 'redux-promise-middleware';
+/*
+    비동기 작업 처리 방법 3 (불편함을 가장 많이 개선) -yarn add redux-pender
+ */
+import penderMiddleware from "redux-pender";
+
 
 /*
     로그 미들웨어 설정 커스터마이징
@@ -30,10 +41,6 @@ import promiseMiddleware from 'redux-promise-middleware';
 const logger = createLogger();
 
 
-const pm = promiseMiddleware({
-    promiseTypeSuffixes: ['PENDING', 'SUCCESS', 'FAILURE'],
-});
-
-const store = createStore(modules, applyMiddleware(logger, /*ReduxThunk*/ pm));
+const store = createStore(modules, applyMiddleware(logger/*ReduxThunk*/ /*pm*/, penderMiddleware()));
 
 export default store;
