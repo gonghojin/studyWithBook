adda5188caea67fbebcea5fc95036400cfdc870e
gonghojin <hjgong@nkia.co.kr>
2019-05-01 16:53:52 +0900

    Add: router와 parameter

--- React/react-router/src/App.js
+++ React/react-router/src/App.js
@@ -1,10 +1,31 @@
 import React from 'react';
+import {Route} from "react-router-dom";
+
+import {Home, About} from "pages";
 
 const App = () => {
-  return (
-      <div>
-        리액트 라우터를 배워보자
-      </div>
-  );
+    return (
+        <div>
+            {
+                /*
+                    exact는 정확한 경로 주소가 들어왔을 때만 보이도록 해준다
+                    만약 exact가 없으면 /about 주소에도 '/'가 존재하기 떄문에 Home component도 출력하게 된다.
+
+                 */
+            }
+            <Route exact path="/" component={Home}/>
+            {/*
+                Url의 params를 지정할 때 - :key
+                 ex) /about/react
+
+                주소 겹침 방지
+                <Route exact path="/about" component={About}/>
+                또는
+                ?로 붙여줌으로써 선택적으로 받을 수 있음
+            */}
+            <Route path="/about/:name?" component={About}/>
+
+        </div>
+    );
 };
 export default App;
