9eceedb949b4358b6d73120247cc2c044d91b29f
gonghojin <hjgong@nkia.co.kr>
2019-05-06 21:43:56 +0900

    Add : Footer Component

--- React/blog/blog-frontend/src/components/common/PageTemplate/PageTemplate.js
+++ React/blog/blog-frontend/src/components/common/PageTemplate/PageTemplate.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import className from 'classnames/bind';
 import Header from 'components/common/Header';
+import Footer from 'components/common/Footer';
 import styles from './PageTemplate.scss';
 
 const cx = className.bind(styles);
 const PageTemplate = () => (
   <div className={cx('page-template')}>
-      <Header/>
+    <Header />
+    <Footer />
   </div>
 );
 
