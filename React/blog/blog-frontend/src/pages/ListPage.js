2762e9ca44d67f51a739745ead738c32e11c17ce
gonghojin <hjgong@nkia.co.kr>
2019-05-08 00:08:45 +0900

    Add : Component(ListWrapper, PostList)

--- React/blog/blog-frontend/src/pages/ListPage.js
+++ React/blog/blog-frontend/src/pages/ListPage.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import PageTemplate from 'components/common/PageTemplate';
+import ListWrapper from 'components/list/ListWrapper/ListWrapper';
+import PostList from 'components/list/PostList/PostList';
 
 const ListPage = () => (
   <div>
     <PageTemplate>
-        List
+      <ListWrapper>
+        <PostList />
+      </ListWrapper>
     </PageTemplate>
   </div>
 );
