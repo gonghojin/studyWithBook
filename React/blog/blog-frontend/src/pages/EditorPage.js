f962dff087cc5da7ac9566d897cd431b61f3b24a
gonghojin <hjgong@nkia.co.kr>
2019-05-11 15:53:48 +0900

    Add : Editor Component(Header, Pane)

--- React/blog/blog-frontend/src/pages/EditorPage.js
+++ React/blog/blog-frontend/src/pages/EditorPage.js
@@ -1,11 +1,14 @@
 import React from 'react';
-import EditorTemplate from 'components/editor/EditorTemplate/EditorTemplate';
+import EditorTemplate from 'components/editor/EditorTemplate';
+import EditorHeader from 'components/editor/EditorHeader';
+import EditorPane from 'components/editor/EditorPane';
+import PreviewPane from 'components/editor/PreviewPane/PreviewPane';
 
 const EditorPage = () => (
   <EditorTemplate
-    header="헤더"
-    editor="에디터"
-    preview="프리뷰"
+    header={<EditorHeader />}
+    editor={<EditorPane />}
+    preview={<PreviewPane />}
   />
 );
 
