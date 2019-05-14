aa33e3d8340410928c5c8c9446d5c167ee891942
gonghojin <hjgong@nkia.co.kr>
2019-05-15 00:39:24 +0900

    Add : 프록시 설정, write_post action In editor module

--- React/blog/blog-frontend/src/store/modules/editor.js
+++ React/blog/blog-frontend/src/store/modules/editor.js
@@ -3,18 +3,24 @@ import { createAction, handleActions } from 'redux-actions';
 import { Map } from 'immutable';
 import { pender } from 'redux-pender';
 
+import * as api from 'lib/api';
+
 // action types
 const INITIALIZE = 'editor/INITIALIZE';
 const CHANGE_INPUT = 'editor/CHANGE_INPUT';
+const WRITE_POST = 'editor/WRITE_POST';
 
 // action creators
 export const initialize = createAction(INITIALIZE);
 export const changeInput = createAction(CHANGE_INPUT);
+export const writePost = createAction(WRITE_POST, api.writePost);
+
 // initial state
 const initialState = Map({
   title: '',
   markdown: '',
   tags: '',
+  postId: null,
 });
 
 // reducer
@@ -24,4 +30,11 @@ export default handleActions({
     const { name, value } = action.payload;
     return state.set(name, value);
   },
+  ...pender({
+    type: WRITE_POST,
+    onSuccess: (state, action) => {
+      const { _id } = action.payload.data;
+      return state.set('postId', _id);
+    },
+  }),
 }, initialState);
