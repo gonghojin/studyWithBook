8be61914a880a24fcbde368e76a5a62867f96b51
gonghojin <yolip51@naver.com>
2020-03-17 01:08:20 +0900

    Fix: 자바로 배우는 리팩토링_플래그2_리팩토링 1회

---
+++ java_refactoring/src/deletion_flag/exam2/Main.java
@@ -0,0 +1,22 @@
+package deletion_flag.exam2;
+
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Iterator;
+
+public class Main {
+	public static void main(String[] args) {
+		try {
+			SimpleDatabase db = new SimpleDatabase(new FileReader("dbfile.txt"));
+			Iterator<String> it = db.iterator();
+			while (it.hasNext()) {
+				String key = it.next();
+				System.out.println("KEY: \"" + key + "\"");
+				System.out.println("VALUE: \"" + db.getValue(key) + "\"");
+				System.out.println();
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+}
